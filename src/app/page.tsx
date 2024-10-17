5b62c3063d4c34ca265690b6bff3b1714b14c06a
Heitors09 <heitorao32@gmail.com>
2024-10-17 19:22:34 -0300

    feat: add services section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Hero from "@/components/hero/hero";
+import Sobre from "@/components/sobre/sobre";
 import BlurFade from "@/components/ui/blur-fade";
+import { PenBox } from "lucide-react";
+import Image from "next/image";
+
 
 
 
@@ -8,31 +12,80 @@ export default function Home() {
   return (
     <>
       <Hero/>
-      <div className="bg-[#0d0d0d] h-auto flex flex-col items-center pt-14 w-full">
-          <div className="flex flex-col items-center gap-8">
-                <BlurFade inView className="text-[#FFC700] font-bold text-2xl pb-1 ">sobre nós ,</BlurFade>
-                <BlurFade inView delay={0.2} className="text-6xl text-white font-bold w-[800px] text-center">criadores de <span className="text-[#FFC700] ">experiencias</span> personalizadas.</BlurFade>
-                <BlurFade inView delay={0.3} className="text-zinc-400  opacity-70 text-center w-[700px]">Preencha corretamente as informações definidas no formulário para que possamos entrar em contato!, possamos entrar em contato!possamos entrar em contato!</BlurFade>
-          </div>
-          <video className="bg-[#1a1a1a] rounded-lg h-[372px] w-[701px] my-5" controls/>
-          <div className="flex  h-auto w-full justify-center mt-[36px] gap-7">
-            <BlurFade delay={0.2} inView className="w-auto flex flex-col gap-4 mt-5  bg-[#1a1a1a] relative border-t-2 rounded-lg border-[#FFC700]  p-5 h-[220px] ">
-             <h2 className="text-[#FFC700] font-bold text-2xl">Eficiência</h2>
-             <p className="text-zinc-400 w-[300px]  text-sm">Preencha corretamente as informações definidas no formulário para que possamos entrar em contato!, possamos entrar em contato!possamos entrar em contato!</p>
-             <p className="text-[#FFC700] font-bold text-sm">saiba mais</p>
-            </BlurFade>
-            <BlurFade  inView className="w-auto flex flex-col gap-4  bg-[#1a1a1a] relative border-t-2 rounded-lg border-[#FFC700]  p-5 h-[220px] ">
-             <h2 className="text-[#FFC700] font-bold text-2xl">Experiencia</h2>
-             <p className="text-zinc-400 w-[300px]  text-sm">Preencha corretamente as informações definidas no formulário para que possamos entrar em contato!, possamos entrar em contato!possamos entrar em contato!</p>
-             <p className="text-[#FFC700] font-bold text-sm">saiba mais</p>
-            </BlurFade>
-            <BlurFade delay={0.2}  inView className="w-auto flex flex-col gap-4 mt-5  bg-[#1a1a1a] relative border-t-2 rounded-lg border-[#FFC700]  p-5 h-[220px] ">
-             <h2 className="text-[#FFC700] font-bold text-2xl">Capacidade Técnica</h2>
-             <p className="text-zinc-400 w-[300px]  text-sm">Preencha corretamente as informações definidas no formulário para que possamos entrar em contato!, possamos entrar em contato!possamos entrar em contato!</p>
-             <p className="text-[#FFC700] font-bold text-sm">saiba mais</p>
-            </BlurFade>
-
-          </div>
+      <Sobre/>
+      <div className="w-full flex flex-col items-center py-20 h-auto bg-[linear-gradient(0deg,rgba(0,0,0,1)_61%,rgba(26,26,26,1)_100%)]">
+                <BlurFade inView className="text-[#FFC700] font-bold text-2xl pb-1 ">serviços ,</BlurFade>
+                <BlurFade inView delay={0.2} className="text-6xl mt-8 text-white font-bold w-[800px] text-center">como posso te <span className="text-[#FFC700] ">ajudar</span> ?</BlurFade>
+          <BlurFade inView delay={0.3} className=" w-full mt-12 h-[300px] flex items-center justify-between text-white px-[137px]">
+             <aside className="flex flex-col gap-3">
+              <div className="h-12 w-12 rounded-full bg-[#FFC700]  bg-opacity-30 flex items-center justify-center">
+                  <PenBox className="text-[#FFC700]"/>
+              </div>
+               <p className="font-bold text-lg text-[#FFC700]">soluções web sob medida</p>
+               <h2 className="font-bold text-5xl">desenvolvimento web</h2>
+               <p className="w-[600px] text-zinc-400">Preencha corretamente as informações definidas no formulário para que possamos entrar em contato!, possamos entrar em contato!possamos entrar em contato!!possamos entrar em contato!</p>
+               <p className="font-bold">saiba mais</p>
+             </aside>
+             <Image
+             alt="web-browser"
+             src='/web-browser.png'
+             width={331}
+             height={194}
+             />
+          </BlurFade>
+          <BlurFade inView delay={0.3} className=" w-full mt-12 h-[300px] flex items-center justify-between text-white px-[137px]">
+          <Image
+             alt="web-browser"
+             src='/web-browser.png'
+             width={331}
+             height={194}
+             />
+             <aside className="flex flex-col gap-3">
+              <div className="h-12 w-12 rounded-full bg-[#FFC700]  bg-opacity-30 flex items-center justify-center">
+                  <PenBox className="text-[#FFC700]"/>
+              </div>
+               <p className="font-bold text-lg text-[#FFC700]">soluções web sob medida</p>
+               <h2 className="font-bold text-5xl">desenvolvimento web</h2>
+               <p className="w-[600px] text-zinc-400">Preencha corretamente as informações definidas no formulário para que possamos entrar em contato!, possamos entrar em contato!possamos entrar em contato!!possamos entrar em contato!</p>
+               <p className="font-bold">saiba mais</p>
+             </aside>
+            
+          </BlurFade>
+          <BlurFade inView delay={0.3} className=" w-full mt-12 h-[300px] flex items-center justify-between text-white px-[137px]">
+             <aside className="flex flex-col gap-3">
+              <div className="h-12 w-12 rounded-full bg-[#FFC700]  bg-opacity-30 flex items-center justify-center">
+                  <PenBox className="text-[#FFC700]"/>
+              </div>
+               <p className="font-bold text-lg text-[#FFC700]">soluções web sob medida</p>
+               <h2 className="font-bold text-5xl">desenvolvimento web</h2>
+               <p className="w-[600px] text-zinc-400">Preencha corretamente as informações definidas no formulário para que possamos entrar em contato!, possamos entrar em contato!possamos entrar em contato!!possamos entrar em contato!</p>
+               <p className="font-bold">saiba mais</p>
+             </aside>
+             <Image
+             alt="web-browser"
+             src='/web-browser.png'
+             width={331}
+             height={194}
+             />
+          </BlurFade>
+          <BlurFade inView delay={0.3} className=" w-full mt-12 h-[300px] flex items-center justify-between text-white px-[137px]">
+          <Image
+             alt="web-browser"
+             src='/web-browser.png'
+             width={331}
+             height={194}
+             />
+             <aside className="flex flex-col gap-3">
+              <div className="h-12 w-12 rounded-full bg-[#FFC700]  bg-opacity-30 flex items-center justify-center">
+                  <PenBox className="text-[#FFC700]"/>
+              </div>
+               <p className="font-bold text-lg text-[#FFC700]">soluções web sob medida</p>
+               <h2 className="font-bold text-5xl">desenvolvimento web</h2>
+               <p className="w-[600px] text-zinc-400">Preencha corretamente as informações definidas no formulário para que possamos entrar em contato!, possamos entrar em contato!possamos entrar em contato!!possamos entrar em contato!</p>
+               <p className="font-bold">saiba mais</p>
+             </aside>
+            
+          </BlurFade>
       </div>
     </>
   );
